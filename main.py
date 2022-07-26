ac2d9f54fdecfb89cd93189edb96bce05745c45f
Colton Chrane <70654301+coltonchrane@users.noreply.github.com>
2022-07-26 18:15:07 +0000

    Attempting to implement an improved method

--- main.py
+++ main.py
@@ -11,78 +11,22 @@ from extract import json_extract
 APIKEY = os.environ['apikey']
 TOKEN = os.environ['TOKEN']
 client = discord.Client()
-date = "21-08-2021"
-client = commands.Bot(command_prefix="!")
+client = commands.Bot(command_prefix='$')
 
-
-#FINDS AND GRABS JSON DATA AND LOADS INTO PY DICTIONARY
-def get_setlist(date):
-  url = "https://api.setlist.fm/rest/1.0/search/setlists?artistMbid=640db492-34c4-47df-be14-96e2cd4b9fe4&date="+date+"&p=1"
-  headers = {
-          'x-api-key': APIKEY ,
-          'Accept': "application/json"
-  }
-  response = requests.get(url, headers=headers)
-  print(response.text)
-
-#JSON INTO PY DICTIONARY
-  json_data = json_extract(json.loads(response.text),"name") 
-  #setlist = json.loads(response.text)   
-  #Format data
-
-  return json_data
-  #return setlist
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    msg = message.content
-    if message.author == client.user:
-        return
-    if msg.startswith("$sl"):
-#DATE VALIDATOR
-      date = msg.split("$sl ",1)[1]  
-      if '/' in date:
-        await message.channel.send("Date must be in DD-MM-YYYY format.")
-      else:
-        day,month,year = date.split('-')
-        isValidDate = True
-        try:
-          datetime.datetime(int(year),int(month), int(day))
-        except ValueError:
-          isValidDate = False
-        if(isValidDate):
 
-#GET SETLIST
-          setlist = get_setlist(date)
+@client.command()
+async def custy(ctx):
+  wook = "kingCust.jpg"
+  await ctx.channel.send(file=discord.File(wook))
+  await ctx.channel.send("King Custy")
 
-#NO SHOW DATA
-          if not setlist:
-            print(setlist)
-            await message.channel.send(file=discord.File("gonfishn.jpg"))
-            await message.channel.send("No setlist for that day")
-          else:
-            print(setlist)
-            await message.channel.send(setlist)
-        else:
-          await message.channel.send("Date must be in DD-MM-YYYY format.")
-      
-#EPIC TROLL       
-    if msg.startswith("$Billycord"):
-      wook = "kingCust.jpg"  
-      await message.channel.send(file=discord.File(wook))
-      await message.channel.send("King Custy")
-
-#HELP
-    if msg.startswith("$help"):
-      await message.channel.send("Type the command followed by the date in this format '$sl DD-MM-YYYY' to get info")
-
-
-#ALTERNATE METHOD
-"""@client.command()
+  
+@client.command()
 async def sl(ctx, *args):
   args = list(args)
   artist=" ".join(args[:-1])
@@ -99,6 +43,7 @@ async def sl(ctx, *args):
 
   headers = {'x-api-key': APIKEY, 'Accept': 'application/json'}
   r = requests.get(f"https://api.setlist.fm/rest/1.0/search/setlists?artistName=%7Bartist%7D&p=1&date=%7Bdate%7D", headers=headers)
+  print(r.text)
   songs = ""
   for setlist in json.loads(r.text)["setlist"]:
     if artist in setlist["artist"]["name"]:
@@ -123,7 +68,14 @@ async def sl(ctx, *args):
     embed.add_field(name='Location', value=location, inline=False)
     embed.set_footer(text="Boy, Man, God, Shit")
     await ctx.send(embed=embed)
-"""
+
+
 #SERVER
 keep_alive()
 client.run(TOKEN)
+
+
+
+
+
+
---
+++ scrap_code.py
@@ -0,0 +1,63 @@
+"""
+#FINDS AND GRABS JSON DATA AND LOADS INTO PY DICTIONARY
+def get_setlist(date):
+    url = "https://api.setlist.fm/rest/1.0/search/setlists?artistMbid=640db492-34c4-47df-be14-96e2cd4b9fe4&date=" + date + "&p=1"
+    headers = {'x-api-key': APIKEY, 'Accept': "application/json"}
+    response = requests.get(url, headers=headers)
+    print(response.text)
+
+    #JSON INTO PY DICTIONARY
+    json_data = json_extract(json.loads(response.text), "name")
+    #setlist = json.loads(response.text)
+    #Format data
+    return json_data
+    #return setlist
+
+"""
+
+'''@client.event
+async def on_message(message):
+    msg = message.content
+    if message.author == client.user:
+        return
+    if msg.startswith("$sl"):
+        #DATE VALIDATOR
+        date = msg.split("$sl ", 1)[1]
+        if '/' in date:
+            await message.channel.send("Date must be in DD-MM-YYYY format.")
+        else:
+            day, month, year = date.split('-')
+            isValidDate = True
+            try:
+                datetime.datetime(int(year), int(month), int(day))
+            except ValueError:
+                isValidDate = False
+            if (isValidDate):
+
+                #GET SETLIST
+                setlist = get_setlist(date)
+
+                #NO SHOW DATA
+                if not setlist:
+                    print(setlist)
+                    await message.channel.send(
+                        file=discord.File("gonfishn.jpg"))
+                    await message.channel.send("No setlist for that day")
+                else:
+                    print(setlist)
+                    await message.channel.send(setlist)
+            else:
+                await message.channel.send("Date must be in DD-MM-YYYY format.")
+
+#EPIC TROLL
+    if msg.startswith("$Billycord"):
+        wook = "kingCust.jpg"
+        await message.channel.send(file=discord.File(wook))
+        await message.channel.send("King Custy")
+
+#HELP
+    if msg.startswith("$help"):
+        await message.channel.send(
+            "Type the command followed by the date in this format '$sl DD-MM-YYYY' to get info"
+        )
+'''
