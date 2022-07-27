00db5eb1390a8fe2963cde9a585b0f63a91bca11
Colton Chrane <70654301+coltonchrane@users.noreply.github.com>
2022-07-27 03:13:22 +0000

    minor updates. Waiting on API upgrade.

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import requests
 import json
 from keep_alive import keep_alive
 import musicbrainzngs
-import time
 
 #CREDENTIALS & VARIABLES
 APIKEY = os.environ['apikey']
@@ -67,13 +66,70 @@ async def set(ctx, *args):
 
 #f'https://api.setlist.fm/rest/1.0/search/setlists?artistName={artist}&p=1&date={date}'
 
+#HELP
+@client.command()
+async def help(ctx):
+  embed = discord.Embed(title="Help", color=discord.Color.blue())
+  embed.add_field(name="$bmfs", value="Follow command with date [MM-DD-YYYY] to see the setlist from that date. i.e. $billy 29-06-2022", inline=False)
+  embed.add_field(name="$dead", value="Follow command with date [MM-DD-YYYY] to see the setlist from that date. i.e. $dead 21-07-1990", inline=False)
+  embed.add_field(name="$custy", value="All hail King Custy", inline=False)
+  embed.add_field(name="$set", value="Follow command with artist and date [MM-DD-YYYY] to see that artsits setlist from that day. i.e. $set widespread panic 25-07-2022", inline=False)
+  await ctx.send(embed=embed)
 
+#Easter egg
+@client.command()
+async def custy(ctx):
+  wook = discord.File("kingCust.jpg")
+  embed = discord.Embed(title="King Custy", color=discord.Color.green())
+  embed.set_image(url="attachment://kingCust.jpg")
+  await ctx.send(embed=embed, file=wook)
+
+#Setlist for the Dead given date
+@client.command()
+async def dead(ctx, args):
+  date= args
+  if not date:
+    embed = discord.Embed(title=":warning: Please provide provide a date [MM-DD-YYYY]!", color=discord.Color.red())
+    embed.set_footer(text="Next Time Will Be Different, Until You Do It Again")
+    await ctx.send(embed=embed)
+    return  
+    
+  url = f"https://api.setlist.fm/rest/1.0/search/setlists?artistMbid=6faa7ca7-0d99-4a5e-bfa6-1fd5037520c6&date={date}&p=1"
+  headers = {'x-api-key': APIKEY, 'Accept': "application/json"}
+  r = requests.get(url, headers=headers)
+  print(r.text)
+  songs = ""
+  artist = "Grateful Dead"
+  for setlist in json.loads(r.text)["setlist"]:
+    if artist in setlist["artist"]["name"]:
+      for set in setlist["sets"]["set"]:
+        try:
+          songs += "-- "+set["name"].replace(':', '') +" --\n"
+        except:
+          pass
+        for song in set["song"]:
+          songs += song["name"]+"\n"
+        songs += "\n"
+
+    location = json.loads(r.text)["setlist"][0]["venue"]["name"] +", " +json.loads(r.text)["setlist"][0]["venue"]["city"]["name"]+", "+json.loads(r.text)["setlist"][0]["venue"]["city"]["state"]
+    embed = discord.Embed(title="Setlist archive", color=discord.Color.gold())
+    embed.add_field(name="Artist", value=artist, inline=True)
+    embed.add_field(name='Date', value=date, inline=True)
+    embed.add_field(name='Songs', value=songs, inline=False)
+    try:
+        embed.add_field(name='Tour Name', value=json.loads(r.text)["setlist"][0]["tour"]["name"], inline=False)
+    except:
+        pass 
+    embed.add_field(name='Location', value=location, inline=False)
+    await ctx.send(embed=embed)
+
+    
 #Setlist for Billy strings given date
 @client.command()
 async def bmfs(ctx, args):
   date= args
   if not date:
-    embed = discord.Embed(title=":warning: Please provide provide a date [DD-06-2022]!", color=discord.Color.red())
+    embed = discord.Embed(title=":warning: Please provide provide a date [MM-DD-YYYY]!", color=discord.Color.red())
     embed.set_footer(text="Next Time Will Be Different, Until You Do It Again")
     await ctx.send(embed=embed)
     return  
@@ -108,66 +164,7 @@ async def bmfs(ctx, args):
     await ctx.send(embed=embed)
   
 
-#NOT WORKING help section. discord commands has a prebuilt help i need to override
-@client.command()
-async def help(ctx):
-  embed = discord.Embed(title="Help", color=discord.Color.blue())
-  embed.add_field(name="$bmfs", value="Follow command with date [MM-DD-YYYY] to see the setlist from that date. i.e. $billy 29-06-2022", inline=False)
-  embed.add_field(name="$dead", value="Follow command with date [MM-DD-YYYY] to see the setlist from that date. i.e. $dead 21-07-1990", inline=False)
-  embed.add_field(name="$custy", value="All hail King Custy", inline=False)
-  embed.add_field(name="$set", value="Follow command with artist and date [MM-DD-YYYY] to see that artsits setlist from that day. i.e. $set widespread panic 25-07-2022", inline=False)
-  await ctx.send(embed=embed)
-
-#Easter egg
-@client.command()
-async def custy(ctx):
-  wook = "kingCust.jpg"
-  await ctx.send(file=discord.File(wook))
-  await ctx.send("King Custy")
-
-#Setlist for Billy strings given date
-@client.command()
-async def dead(ctx, args):
-  date= args
-  if not date:
-    embed = discord.Embed(title=":warning: Please provide provide a date [DD-06-2022]!", color=discord.Color.red())
-    embed.set_footer(text="Next Time Will Be Different, Until You Do It Again")
-    await ctx.send(embed=embed)
-    return  
-    
-  url = f"https://api.setlist.fm/rest/1.0/search/setlists?artistMbid=6faa7ca7-0d99-4a5e-bfa6-1fd5037520c6&date={date}&p=1"
-  headers = {'x-api-key': APIKEY, 'Accept': "application/json"}
-  r = requests.get(url, headers=headers)
-  print(r.text)
-  songs = ""
-  artist = "Grateful Dead"
-  for setlist in json.loads(r.text)["setlist"]:
-    if artist in setlist["artist"]["name"]:
-      for set in setlist["sets"]["set"]:
-        try:
-          songs += "-- "+set["name"].replace(':', '') +" --\n"
-        except:
-          pass
-        for song in set["song"]:
-          songs += song["name"]+"\n"
-        songs += "\n"
-
-    location = json.loads(r.text)["setlist"][0]["venue"]["name"] +", " +json.loads(r.text)["setlist"][0]["venue"]["city"]["name"]+", "+json.loads(r.text)["setlist"][0]["venue"]["city"]["state"]
-    embed = discord.Embed(title="Setlist archive", color=discord.Color.gold())
-    embed.add_field(name="Artist", value=artist, inline=True)
-    embed.add_field(name='Date', value=date, inline=True)
-    embed.add_field(name='Songs', value=songs, inline=False)
-    try:
-        embed.add_field(name='Tour Name', value=json.loads(r.text)["setlist"][0]["tour"]["name"], inline=False)
-    except:
-        pass 
-    embed.add_field(name='Location', value=location, inline=False)
-    await ctx.send(embed=embed)
-  
-
-  
-  
-  #SERVER
+#SERVER
 keep_alive()
 client.run(TOKEN)
 
